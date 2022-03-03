364bd88c9596a3bd8ea07fd1a782825394c5e53f
Pneha1234 <nehapandey408@gmail.com>
2022-03-03 19:04:21 +0545

    add variables

--- Program.cs
+++ Program.cs
@@ -9,6 +9,18 @@ namespace TestProject
             Console.WriteLine("Hello World!");
 
             Console.ReadLine();
+
+            //declaring variables
+
+            string characterName = "John";
+            int charcaterAge = 35;
+
+            Console.WriteLine("There once was a man named" + characterName);
+            Console.WriteLine("He was" + charcaterAge + "years old!");
+            Console.WriteLine("He really liked the name" + characterName);
+            Console.WriteLine("But didn't liked beimg" + charcaterAge);
+
+            Console.ReadLine();
         }
     }
 }
